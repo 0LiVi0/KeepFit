c980a143713008aa3205a9411e6002018652c3c3
muralV <vitalii.mural.sa.2021@lpnu.ua>
2022-05-31 00:52:17 +0300

    Fourth commit. Map integration

--- app/src/main/java/com/example/myapplication/MainMenu.java
+++ app/src/main/java/com/example/myapplication/MainMenu.java
@@ -5,17 +5,61 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+
 public class MainMenu extends Fragment  {
+
+    private MapView mapView;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.main_menu, container, false);
+        Mapbox.getInstance(getContext().getApplicationContext(), "pk.eyJ1IjoibXVydml0YWxpayIsImEiOiJjbDN0OHludmUxNXFkM2psdGNyNjcwN21xIn0._yHfnXYvptI7oAemYvQg3g");
+        mapView = rootView.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
 
 
         return rootView;
-
     }
 
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
 }
